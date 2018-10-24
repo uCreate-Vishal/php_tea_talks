3bd416bc3997d76ac1368d5fd1574ce795390db3
Sandeep Kaur <sandeepkaur@ucreate.co.in>
2018-10-24 18:21:33 +0530

    POC for reference key added

--- POCs/keys/app/Models/User.php
+++ POCs/keys/app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','user_id'
     ];
 
     /**
@@ -27,4 +27,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function profile() {
+        return $this->hasOne('App\Models\Profile', 'user_id', 'user_id');
+    }
 }
